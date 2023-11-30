327dd387c9c6c3caa2335ecf62478f585ac66f0c
puffer <kevin@alavik.se>
2023-11-30 07:42:48 +0100

    feat: Added destroy_nighterm

--- src/nighterm/nighterm.h
+++ src/nighterm/nighterm.h
@@ -2,11 +2,12 @@
 #define NIGHTERM_H_
 
 struct Terminal {
-  int rows;
-  int cols;
-  char* textBuffer;
+    int rows;
+    int cols;
+    char** textBuffer;
 };
 
 struct Terminal* init_nighterm(int rows, int cols);
+void destroy_nighterm(struct Terminal* term);
 
 #endif // NIGHTERM_H_
