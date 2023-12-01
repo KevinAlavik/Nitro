e0290c57e5a13c33e0907d920f22f28d85932ed7
puffer <kevin@alavik.se>
2023-12-01 20:10:48 +0100

    feat: Made the logger look different

--- src/utilities/logger.h
+++ src/utilities/logger.h
@@ -18,37 +18,34 @@ void log(LogLevel level, const char* message) {
 
     switch (level) {
         case INFO:
-            level_str = "INFO";
+            level_str = "Info";
             r = 0; g = 0; b = 255; // Blue
             break;
         case WARNING:
-            level_str = "WARNING";
+            level_str = "Warning";
             r = 255; g = 165; b = 0; // Orange
             break;
         case ERROR:
-            level_str = "ERROR";
+            level_str = "Error";
             r = 255; g = 0; b = 0; // Red
             break;
         case PANIC:
-            level_str = "PANIC";
+            level_str = "Panic";
             r = 255; g = 0; b = 0; // Red
             break;
         case OK:
-            level_str = "OK";
+            level_str = "Ok";
             r = 0; g = 255; b = 0; // Green
             break;
         default:
-            level_str = "UNKNOWN";
+            level_str = "Unknown";
             r = g = b = 255; // Default color
             break;
     }
-
-    printf("[ ");
     nighterm_set_char_fg(r,g,b);
     printf(level_str);
     nighterm_set_char_fg(255,255,255);//NOTE: this is kinda wrong, look here if the color is misteriously resetting
-    printf(" ] ");
-    printf("%s\n",message);
+    printf(": %s\n",message);
 }
 
 #endif // LOGGER_H_
